111fbf99927017980d77e5206f4c625df54f1312
Maxime26s <35577093+Maxime26s@users.noreply.github.com>
2022-10-07 20:19:27 -0400

    fps

---
+++ Assets/Scripts/CinemachinePOVExtension.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CinemachinePOVExtension : CinemachineExtension
+{
+    [SerializeField]
+    private float cameraSpeed = 10f;
+    [SerializeField]
+    private float clampAngle = 80f;
+
+    private Vector3 startingRotation;
+
+
+    protected override void PostPipelineStageCallback(CinemachineVirtualCameraBase vcam, CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+    {
+        if (vcam.Follow)
+        {
+            if (stage == CinemachineCore.Stage.Aim)
+            {
+                if(startingRotation == null)
+                {
+                    startingRotation = transform.localRotation.eulerAngles;
+                }
+
+                Vector2 deltaInput = InputManager.Instance.GetMouseDelta();
+                startingRotation += (Vector3)(deltaInput * Time.deltaTime * cameraSpeed);
+                startingRotation.y = Mathf.Clamp(startingRotation.y, -clampAngle, clampAngle);
+                state.RawOrientation = Quaternion.Euler(-startingRotation.y, startingRotation.x, 0f);
+            }
+        }
+    }
+}
