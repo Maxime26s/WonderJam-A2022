e9ffaf3b781cefac44c2a5f6fbadf0d009539854
Maxime26s <35577093+Maxime26s@users.noreply.github.com>
2022-10-08 12:52:08 -0400

    bitch lasagna

--- Assets/Scripts/DialogueBoxManager.cs
+++ Assets/Scripts/DialogueBoxManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -10,10 +11,11 @@ public class DialogueBoxManager : MonoBehaviour
     public AudioSource audioSource;
     public TextMeshProUGUI textMeshProUGUI;
 
-    public Sprite mouthClosed;
-    public Sprite mouthOpened;
+    public DialogueTemplate currentDialogue;
+    public DialogueVoiceLine currentVoiceLine;
+    public DialogueFace currentFace;
 
-    public string text;
+    public bool isPlaying = false;
 
     private float clipLoudness;
     private float[] clipSampleData;
@@ -26,10 +28,25 @@ public class DialogueBoxManager : MonoBehaviour
     private float lastWordTime;
     public float wordSpeed = 1.25f;
 
+    private Vector2 faceDefaultAnchorX;
+    float deltaFace;
+    private Vector2 textDefaultAnchorX;
+    float deltaText;
+
     // Start is called before the first frame update
     void Start()
     {
         clipSampleData = new float[sampleDataLength];
+
+        faceDefaultAnchorX = new Vector2(imageHolder.rectTransform.anchorMin.x, imageHolder.rectTransform.anchorMax.x);
+        Debug.Log(faceDefaultAnchorX);
+        deltaFace = faceDefaultAnchorX.y - faceDefaultAnchorX.x;
+        Debug.Log(deltaFace);
+        textDefaultAnchorX = new Vector2(textMeshProUGUI.rectTransform.anchorMin.x, textMeshProUGUI.rectTransform.anchorMax.x);
+        Debug.Log(textDefaultAnchorX);
+        deltaText = textDefaultAnchorX.y - textDefaultAnchorX.x;
+        Debug.Log(deltaText);
+
         StartDialogue();
     }
 
@@ -38,6 +55,7 @@ public class DialogueBoxManager : MonoBehaviour
     {
         if (audioSource.isPlaying)
         {
+            isPlaying = true;
             currentUpdateTime += Time.deltaTime;
             if (currentUpdateTime >= updateStep)
             {
@@ -51,10 +69,10 @@ public class DialogueBoxManager : MonoBehaviour
                 clipLoudness /= sampleDataLength; //clipLoudness is what you are looking for
             }
 
-            if (clipLoudness > 0.175f)
-                imageHolder.sprite = mouthOpened;
+            if (clipLoudness > currentVoiceLine.closedMouthThreshold)
+                imageHolder.sprite = currentFace.mouthOpened;
             else
-                imageHolder.sprite = mouthClosed;
+                imageHolder.sprite = currentFace.mouthClosed;
 
 
             float fastLetterPerSecond = letterPerSecond * wordSpeed;
@@ -63,24 +81,78 @@ public class DialogueBoxManager : MonoBehaviour
 
             if (audioSource.time - lastWordTime <= fastWordTime)
             {
-                textMeshProUGUI.text = text.Substring(0, Mathf.CeilToInt(lastWordTime * letterPerSecond + fastLetterPerSecond * (audioSource.time - lastWordTime)));
+                textMeshProUGUI.text = currentVoiceLine.text.Substring(0, Mathf.Clamp(Mathf.CeilToInt(lastWordTime * letterPerSecond + fastLetterPerSecond * (audioSource.time - lastWordTime)), 0, currentVoiceLine.text.Length-1));
             }
             else if (audioSource.time > lastWordTime + wordTime)
             {
                 lastSpaceIndex = nextSpaceIndex;
-                nextSpaceIndex = text.IndexOf(' ', lastSpaceIndex + 1);
+                nextSpaceIndex = currentVoiceLine.text.IndexOf(' ', lastSpaceIndex + 1);
                 lastWordTime = audioSource.time;
             }
-       }
+        }
+        else if (isPlaying)
+        {
+            StartCoroutine(WaitThenPlay(currentDialogue.pauseLengthBetweenVoiceLines));
+        }
     }
-    public void StartDialogue(AudioClip dialogue = null)
+    public void StartDialogue(DialogueTemplate dialogueTemplate = null)
     {
-        if (dialogue != null)
-            audioSource.clip = dialogue;
-        letterPerSecond = text.Length / audioSource.clip.length;
+        if (dialogueTemplate != null)
+            currentDialogue = dialogueTemplate;
+        currentDialogue.voiceLines = new Queue<DialogueVoiceLine>(currentDialogue.voiceLinesList);
+        Debug.Log(currentDialogue.voiceLines);
+        PlayNextVoiceLine();
+    }
+
+    public void PlayNextVoiceLine()
+    {
+        if (!currentDialogue.voiceLines.TryDequeue(out currentVoiceLine))
+        {
+            isPlaying = false;
+            DialogueFinished?.Invoke();
+            return;
+        }
+
+        currentFace = currentVoiceLine.face;
+
+        switch (currentVoiceLine.faceLocation)
+        {
+            case Location.Left:
+                imageHolder.rectTransform.anchorMin = new Vector2(faceDefaultAnchorX.x, imageHolder.rectTransform.anchorMin.y);
+                imageHolder.rectTransform.anchorMax = new Vector2(faceDefaultAnchorX.y, imageHolder.rectTransform.anchorMax.y);
+
+                textMeshProUGUI.rectTransform.anchorMin = new Vector2(textDefaultAnchorX.x, textMeshProUGUI.rectTransform.anchorMin.y);
+                textMeshProUGUI.rectTransform.anchorMax = new Vector2(textDefaultAnchorX.y, textMeshProUGUI.rectTransform.anchorMax.y);
+                
+                break;
+            case Location.Right:
+                imageHolder.rectTransform.anchorMin = new Vector2(textDefaultAnchorX.y - deltaFace, imageHolder.rectTransform.anchorMin.y);
+                imageHolder.rectTransform.anchorMax = new Vector2(textDefaultAnchorX.y, imageHolder.rectTransform.anchorMax.y);
+
+                textMeshProUGUI.rectTransform.anchorMin = new Vector2(faceDefaultAnchorX.x, textMeshProUGUI.rectTransform.anchorMin.y);
+                textMeshProUGUI.rectTransform.anchorMax = new Vector2(faceDefaultAnchorX.x + deltaText, textMeshProUGUI.rectTransform.anchorMax.y);
+
+                break;
+        }
+
+
+        audioSource.clip = currentVoiceLine.voiceLine;
+        letterPerSecond = currentVoiceLine.text.Length / audioSource.clip.length;
         lastSpaceIndex = 0;
-        nextSpaceIndex = text.IndexOf(' ', lastSpaceIndex);
+        nextSpaceIndex = currentVoiceLine.text.IndexOf(' ', lastSpaceIndex);
+        if (nextSpaceIndex == -1)
+            nextSpaceIndex = currentVoiceLine.text.Length;
         lastWordTime = 0;
         audioSource.Play();
     }
+
+    IEnumerator WaitThenPlay(float waitTime)
+    {
+        isPlaying = false;
+        yield return new WaitForSeconds(waitTime);
+        PlayNextVoiceLine();
+    }
+
+    public delegate void DialogueFinishedHandler();
+    public event DialogueFinishedHandler DialogueFinished;
 }
